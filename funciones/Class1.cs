e5e57535b2254608e394f08690fa787d619b1d4a
Jackemp <jackpaalomino@gmail.com>
2024-11-28 00:01:33 -0500

    first commit

---
+++ funciones/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funciones
+{
+    internal class Class1
+    {
+        int aa;
+       Console.WriteLine("MENSAJE DE JACK")
+            aa = int.Parse(Console.ReadLine());
+    }
+}
